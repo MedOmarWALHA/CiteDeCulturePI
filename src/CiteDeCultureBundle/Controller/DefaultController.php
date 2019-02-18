4ebf768624d160a511cd71e5479c44e6ee5a1021
medomarwalha <medomar.walha@esprit.tn>
2019-02-18 20:31:59 +0100

    Basic structure

--- src/CiteDeCultureBundle/Controller/DefaultController.php
+++ src/CiteDeCultureBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace CiteDeCultureBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
@@ -11,6 +12,10 @@ class DefaultController extends Controller
         return $this->render('CiteDeCultureBundle:Default:index.html.twig');
     }
 
+    /**
+     * @IsGranted("ROLE_ADMIN")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function backAction()
     {
         return $this->render('CiteDeCultureBundle:Default:back.html.twig');
