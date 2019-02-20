51843fdeef28e8e599133066518c3e4854173c42
Novack18 <salem.mahbouli@esprit.tn>
2019-02-20 14:18:06 +0100

    Default Changelist

---
+++ src/FoireBundle/Controller/StandController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace FoireBundle\Controller;
+
+use FoireBundle\Entity\Foire;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Foire controller.
+ *
+ */
+class StandController extends Controller
+{
+    
+
+}
